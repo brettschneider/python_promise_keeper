6644d8ae7850afda368a68c70873bdc36a7553b2
Steve Brettschneider <steve@bluehousefamily.com>
2017-03-03 23:44:08 -0500

    Added some thread safety to the Promise class.

--- promise_keeper.py
+++ promise_keeper.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from threading import Event, Thread
+from threading import Event, Lock, Thread
 from Queue import Queue, Empty
 from datetime import datetime
 
@@ -11,11 +11,11 @@ class PromiseKeeper(object):
         self._threads = []
         self._work_queue = Queue()
 
-    def submit(self, task, args=[], kwargs={}, callback=None):
+    def submit(self, task, args=[], kwargs={}, notify=None):
         """
         Submit a task to be scheduled in the PromiseKeeper
         """
-        promise = Promise(task, args, kwargs, callback)
+        promise = Promise(task, args, kwargs, notify)
         self._work_queue.put(promise)
         return promise
 
@@ -64,53 +64,122 @@ class _PromiseWorkerThread(Thread):
                 promise = self._work_queue.get_nowait()
             except Empty:
                 continue
-            promise.started_on = datetime.now()
+            promise._set_started_on(datetime.now())
             try:
-                promise.result = promise.task(*promise.args, **promise.kwargs)
+                promise._set_result(promise._task(*promise._args, **promise._kwargs))
             except Exception as e:
-                promise.exception = e
-            if promise.callback is not None:
+                promise._set_exception(e)
+            if promise._notify is not None:
                 try:
-                    promise.callback(promise)
+                    promise._notify(promise)
                 except:
                     pass
-            promise.completed_on = datetime.now()
+            promise._set_completed_on(datetime.now())
             self._work_queue.task_done()
 
 
 class Promise(object):
     """A promise of future results"""
 
-    def __init__(self, task, args, kwargs, callback):
-        self.task = task
-        self.args = args
-        self.kwargs = kwargs
-        self.exception = None
-        self.result = None
-        self.started_on = None
-        self.completed_on = None
-        self.callback = callback
+    def __init__(self, task, args, kwargs, notify):
+        self._task = task
+        self._args = args
+        self._kwargs = kwargs
+        self._exception = None
+        self._result = None
+        self._started_on = None
+        self._completed_on = None
+        self._notify = notify 
+        self._lock = Lock()
 
     def __repr__(self):
         if self.is_ready():
             return "<Promise: result=%s, exception=%s>" % \
-                (self.result, self.exception)
+                (self._result, self._exception)
         if self.has_started():
-            return "<Promise: running since %s>" % self.started_on
+            return "<Promise: running since %s>" % self._started_on
         else:
             return "<Promise: waiting>"
 
     def is_ready(self):
-        return self.completed_on is not None
+        self._lock.acquire()
+        completed_on = self._completed_on
+        self._lock.release()
+        return completed_on is not None
 
     def has_started(self):
-        return self.started_on is not None
+        return self.get_started_on() is not None
 
-    def execution_time(self):
-        if self.completed_on is None:
+    def get_execution_time(self):
+        """
+        Returns the time_delta for execution if completed.  Otherwise returns
+        None.
+        """
+        if not self.is_ready():
             return None
         else:
-            return self.completed_on - self.started_on
+            self._lock.acquire()
+            exec_time = self._completed_on - self._started_on
+            self._lock.release()
+            return exec_time
+
+    def get_started_on(self):
+        """
+        Returns the datetime of when the task was started, or None if it's still
+        waiting.
+        """
+        self._lock.acquire()
+        started_on = self._started_on
+        self._lock.release()
+        return started_on
+
+    def _set_started_on(self, started_on):
+        """Used by the worker thread to set the started_on value."""
+        self._lock.acquire()
+        self._started_on = started_on
+        self._lock.release()
+
+    def get_completed_on(self):
+        """
+        Returns the datetime of when the task was completed, or None if it's
+        still waiting or running.
+        """
+        self._lock.acquire()
+        completed_on = self._completed_on
+        self._lock.release()
+        return completed_on
+
+    def _set_completed_on(self, completed_on):
+        """Used by the worker thread to set the completed_on value."""
+        self._lock.acquire()
+        self._completed_on = completed_on
+        self._lock.release()
+
+    def get_result(self):
+        """Returns the result or None if it's not ready."""
+        self._lock.acquire()
+        result = self._result
+        self._lock.release()
+        return result
+
+    def _set_result(self, result):
+        """Used by the worker thread to set the result."""
+        self._lock.acquire()
+        self._result = result
+        self._lock.release()
+
+    def get_exception(self):
+        """Returns the exception or None if there isn't one."""
+        self._lock.acquire()
+        exception = self._exception
+        self._lock.release()
+        return exception
+
+    def _set_exception(self, exception):
+        """Used by the worker thread to set the exception."""
+        self._lock.acquire()
+        self._exception = exception
+        self._lock.release()
 
 
 class PromiseKeeperStateError(Exception):
