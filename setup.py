192abd14ecf47d42ea807ee9d9ed4820ff4d3991
Steve Brettschneider <steve.brettschneider@kroger.com>
2017-03-08 15:35:08 -0500

    Prep'ing for pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'promise_keeper',
+    package = ['promise_keeper'],
+    version = '0.1',
+    description = 'A simple async processing helper',
+    author = 'Steve Brettschneider',
+    author_email = 'steve@bluehousefamily.com',
+    url = 'https://github.com/brettschneider/python_promise_keeper',
+    download_url = 'https://github.com/brettschneider/python_promise_keeper/archive/0.1.tar.gz',
+    keywords = ['promise', 'async', 'threading', 'aynchronous', 'simple'],
+    classifiers = [],
+)
