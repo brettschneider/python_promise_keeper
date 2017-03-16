f32b8bf92409d73088417c755209c84eb2c674d3
Steve Brettschneider <steve.brettschneider@kroger.com>
2017-03-16 15:22:23 -0400

    Updated setup.py, added README.rst

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import setup
 setup(
     name = 'promise_keeper',
     py_modules = ['promise_keeper'],
-    version = '0.2',
+    version = '0.3',
     description = 'A simple async processing helper',
     author = 'Steve Brettschneider',
     author_email = 'steve@bluehousefamily.com',
     url = 'https://github.com/brettschneider/python_promise_keeper',
-    download_url = 'https://github.com/brettschneider/python_promise_keeper/archive/0.2.tar.gz',
+    download_url = 'https://github.com/brettschneider/python_promise_keeper/archive/0.3.tar.gz',
     keywords = ['promise', 'async', 'threading', 'aynchronous', 'simple'],
     classifiers = [],
 )
