92c56126f794f9fbc4163fcaf6067b49b8c7e81c
Rémi Desgrange <remi.desgrange@camptocamp.com>
2020-02-14 10:17:11 +0100

    make it compatible with python3 and add type hint

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from setuptools import setup
 
 setup(
-    name = 'promise_keeper',
-    py_modules = ['promise_keeper'],
-    version = '0.3',
-    description = 'A simple async processing helper',
-    author = 'Steve Brettschneider',
-    author_email = 'steve@bluehousefamily.com',
-    url = 'https://github.com/brettschneider/python_promise_keeper',
-    download_url = 'https://github.com/brettschneider/python_promise_keeper/archive/0.3.tar.gz',
-    keywords = ['promise', 'async', 'threading', 'aynchronous', 'simple'],
-    classifiers = [],
+    name="promise_keeper",
+    py_modules=["promise_keeper"],
+    version="0.3",
+    description="A simple async processing helper",
+    author="Steve Brettschneider",
+    author_email="steve@bluehousefamily.com",
+    url="https://github.com/brettschneider/python_promise_keeper",
+    download_url="https://github.com/brettschneider/python_promise_keeper/archive/0.3.tar.gz",
+    keywords=["promise", "async", "threading", "aynchronous", "simple"],
+    classifiers=[],
 )
