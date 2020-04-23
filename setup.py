4114132645313dbc188a3cde8461faedc280d7f2
Steve Brettschneider <steve@bluehousefamily.com>
2020-04-23 09:00:17 -0400

    Small build/deploy stuff.

--- setup.py
+++ setup.py
@@ -2,15 +2,19 @@
 
 from setuptools import setup
 
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="promise_keeper",
     py_modules=["promise_keeper"],
     version="0.4",
     description="A simple async processing helper",
+    long_description=long_description,
     author="Steve Brettschneider",
     author_email="steve@bluehousefamily.com",
     url="https://github.com/brettschneider/python_promise_keeper",
     download_url="https://github.com/brettschneider/python_promise_keeper/archive/0.4.tar.gz",
     keywords=["promise", "async", "threading", "aynchronous", "simple"],
-    classifiers=[],
+    classifiers=[]
 )
